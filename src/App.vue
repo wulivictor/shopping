6ce474c7e61de64248d889861aefd96dbb9d92d0
freedream688 <1351698648@qq.com>
2018-04-22 11:20:46 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <v-head></v-head>
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import header from './components/header/header'
+
+  export default {
+  name: 'App',
+  components : {
+    'v-head':header,
+
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>我是标题</div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        name: 'v-header'
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
