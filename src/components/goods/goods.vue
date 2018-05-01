cce58c123d0388324366546cb2b4065c37efea78
freedream688 <1351698648@qq.com>
2018-05-02 00:45:18 +0800

    开始商品部分

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,10 +1,61 @@
 <template>
-<div class="goods">I am goods</div>
+<div class="goods">
+  <div class="menu-wapper">
+    <ul v-show="goods">
+      <li v-for="(item,index) in goods" class="meun-option">
+          <span class="option-content"><span class="icon" v-show="item.type>0" :class=classMap[item.type]></span>
+           {{item.name}}</span>
+      </li>
+    </ul>
+  </div>
+  <div class="goods-wapper">
+    <ul>
+      <li  v-for="item in goods" class="food-list">
+        <h1>{{item.name}}</h1>
+        <ul>
+          <li v-for="food in item.foods" class="food-item">
+            <div class="food-icon">
+              <img :src="food.icon">
+            </div>
+            <div class="food-content">
+              <h1>{{food.name}}</h1>
+
+            </div>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+
 </template>
 
 <script type="text/ecmascript-6">
+const ERRNO = 0
 export default {
-  name: 'goods'
+  name: 'goods',
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      goods: []
+    }
+  },
+  created () {
+    // 定义icon种类
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+    // goods数据
+    this.$http.get('api/goods').then((response) => {
+      response = response.data
+      if (response.errno === ERRNO) {
+        this.goods = response.data
+        console.log(this.goods)
+      }
+    })
+  }
 }
 </script>
 
