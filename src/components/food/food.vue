241aed550ea37213a3487b92e9e35e257fca8582
Administrator <1351698648@qq.com>
2018-06-05 14:15:00 +0800

    1、开始适配大屏幕适配   >475px 如iPad Pro

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,66 +1,70 @@
 <template>
-  <transition name="fade">
-    <div v-show="showFlag" class="food" ref="foodWapper" id="foodWapper"> <!-- @touchstart='touchStart' @touchend='touchEnd' -->
-      <div class="page-scroll">
-        <div class="food-detail">
-          <img v-bind:src="food.image" class="food-image">
-          <div class="food-msg">
-            <h2>{{food.name}}</h2>
-            <p class="desc"><span class="sellcount">月售{{food.sellCount}}份</span><span class="rating">好评率{{food.rating}}%</span></p>
-            <div class="cart">
-              <div class="price">
-              <span class="nowprice">
-                <span style="font-size: 10px;font-weight: normal">￥</span>
-                {{food.price}}
-              </span>
-                <span class="oldprice" v-show="food.oldPrice">
-                <span style="font-weight: normal">￥</span>
-                {{food.oldPrice}}
-              </span>
-              </div>
-              <div class="addcart">
-                <div class="addbtn" @click="addcart" v-bind:hidden="food.count || !food.count === 0">加入购物车</div>
-              </div>
-            </div>
+  <div v-show="showFlag" class="food" ref="foodWapper" id="foodWapper"> <!-- @touchstart='touchStart' @touchend='touchEnd' -->
+    <div class="page-scroll">
+      <div class="food-detail">
+        <div class="img-show">
+          <div class="picBackground">
+          </div>
+          <div class="picWapper">
+            <img v-bind:src="food.image" class="food-image">
           </div>
         </div>
-        <div class="food-intro" v-show="food.info">
-          <h2>商品介绍</h2>
-          <p class="desc">{{food.info}}</p>
-        </div>
-        <div class="food-rating" v-show="food.ratings">
-          <div class="header">
-            <h2>商品评论</h2>
-            <div class="btn-group">
-              <div class="all" @click="changeRate('all')">全部<span class="ratingtotal"></span></div>
-              <div class="recommand" @click="changeRate('recommand')">推荐</div>
-              <div class="gag" @click="changeRate('gag')">吐槽</div>
+        <div class="food-msg">
+          <h2>{{food.name}}</h2>
+          <p class="desc"><span class="sellcount">月售{{food.sellCount}}份</span><span class="rating">好评率{{food.rating}}%</span></p>
+          <div class="cart">
+            <div class="price">
+            <span class="nowprice">
+              <span style="font-size: 10px;font-weight: normal">￥</span>
+              {{food.price}}
+            </span>
+              <span class="oldprice" v-show="food.oldPrice">
+              <span style="font-weight: normal">￥</span>
+              {{food.oldPrice}}
+            </span>
             </div>
-            <div class="ratingoption">
-              <i  class="icon-check_circle" v-bind:class="[rateoption.showcontent ? 'checked' : '']" @click="showContent()"></i><span>只看有内容的评论</span>
+            <div class="addcart">
+              <div class="addbtn" @click="addcart" v-bind:hidden="food.count || !food.count === 0">加入购物车</div>
             </div>
-          </div>
-          <div class="content">
-            <ul>
-              <li class="rate-item" v-for="(rate, index) in selectrating" v-bind:key="index">
-                <div class="rate-msg">
-                  <div class="rate-time">{{rate.rateTime | formatDate}}</div>
-                  <div class="rate-headimg" v-bind:style="{backgroundImage:'url(' + rate.avatar + ')'}"></div>
-                  <div class="rate-name">{{rate.username}}</div>
-                </div>
-                <div class="rate-content" >
-                  <i v-bind:class="[rate.rateType === 0 ? 'icon-thumb_up' : 'icon-thumb_down']"></i>
-                  <span class="rate-text" v-show="rate.text !== ''">{{rate.text}}</span>
-                  <span class="rate-text" v-show="rate.text === '' && rate.rateType === 0">GOOD</span>
-                  <span class="rate-text" v-show="rate.text === '' && rate.rateType === 1">NOT GOOD</span>
-                </div>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
+      <div class="food-intro" v-show="food.info">
+        <h2>商品介绍</h2>
+        <p class="desc">{{food.info}}</p>
+      </div>
+      <div class="food-rating" v-show="food.ratings">
+        <div class="header">
+          <h2>商品评论</h2>
+          <div class="btn-group">
+            <div class="all" @click="changeRate('all')">全部<span class="ratingtotal"></span></div>
+            <div class="recommand" @click="changeRate('recommand')">推荐</div>
+            <div class="gag" @click="changeRate('gag')">吐槽</div>
+          </div>
+          <div class="ratingoption">
+            <i  class="icon-check_circle" v-bind:class="[rateoption.showcontent ? 'checked' : '']" @click="showContent()"></i><span>只看有内容的评论</span>
+          </div>
+        </div>
+        <div class="content">
+          <ul>
+            <li class="rate-item" v-for="(rate, index) in selectrating" v-bind:key="index">
+              <div class="rate-msg">
+                <div class="rate-time">{{rate.rateTime | formatDate}}</div>
+                <div class="rate-headimg" v-bind:style="{backgroundImage:'url(' + rate.avatar + ')'}"></div>
+                <div class="rate-name">{{rate.username}}</div>
+              </div>
+              <div class="rate-content" >
+                <i v-bind:class="[rate.rateType === 0 ? 'icon-thumb_up' : 'icon-thumb_down']"></i>
+                <span class="rate-text" v-show="rate.text !== ''">{{rate.text}}</span>
+                <span class="rate-text" v-show="rate.text === '' && rate.rateType === 0">GOOD</span>
+                <span class="rate-text" v-show="rate.text === '' && rate.rateType === 1">NOT GOOD</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  </transition>
+  </div>
 </template>
 <script type="text/ecmascript-6">
 import {formatDate} from '../../common/js/date'
@@ -143,9 +147,6 @@ export default {
         click: true,
         probeType: 3
       })
-      // this.foodScroll.on('scroll', (pos) => {
-      //   console.log(pos)
-      // })
     },
     show () {
       this.showFlag = true
--- src/components/rating/rating.vue
+++ src/components/rating/rating.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="rating">I am rating</div>
+  <div class="rating">{{seller}}</div>
 </template>
 
 <script type="text/ecmascript-6">
+import shopcart from '../../components/shopcart/shopcart.vue'
 export default {
-  name: 'rating'
+  name: 'rating',
+  data () {
+    return {
+
+    }
+  },
+  props: {
+    seller: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  components: {
+    shopcart
+  }
 }
 </script>
-
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
 </style>
