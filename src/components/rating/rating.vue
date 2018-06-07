6cba979f570a4c2c87b90a6d0b5740e7fc9af162
freedream688 <1351698648@qq.com>
2018-06-07 10:31:44 +0800

    1、完成app大致布局 2、下一步需要购物车组件 3、看看是否还有别的需求

--- src/components/rating/rating.vue
+++ src/components/rating/rating.vue
@@ -1,72 +1,72 @@
 <template>
 <div class="rating">
-  <div class="rating-content" ref="rateWapper">
-    <div class="overview">
-      <div class="overview-left">
-        <p class="score-number">{{seller.score}}</p>
-        <p class="score-title">综合评分</p>
-        <p class="over">高于周边商家{{seller.rankRate}}%</p>
-      </div>
-      <div class="overview-right">
-        <div class="food-score">
-          <div class="text">服务态度</div>
-          <star class="star" :size='36' :score=seller.serviceScore></star>
-          <div class="number">{{seller.serviceScore}}</div>
+    <div class="rating-content" >
+      <div class="overview">
+        <div class="overview-left">
+          <p class="score-number">{{seller.score}}</p>
+          <p class="score-title">综合评分</p>
+          <p class="over">高于周边商家{{seller.rankRate}}%</p>
         </div>
-        <div class="service-score">
-          <div class="text">食物评分</div>
-          <star class="star" :size='36' :score=seller.foodScore></star>
-          <div class="number">{{seller.serviceScore}}</div>
-        </div>
-        <div class="deliverytime">
-          <div class="text">送达时间</div>
-          <div class="time">{{seller.deliveryTime}}分钟</div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- 评论区域  -->
-  <div class="food-rating">
-    <div class="header">
-      <div class="btn-group">
-        <div class="all" @click="changeRate('all')">全部<span class="ratingtotal"></span></div>
-        <div class="recommand" @click="changeRate('recommand')">推荐</div>
-        <div class="gag" @click="changeRate('gag')">吐槽</div>
-      </div>
-      <div class="ratingoption">
-        <i  class="icon-check_circle" v-bind:class="[rateoption.showcontent ? 'checked' : '']" @click="showContent()"></i><span>只看有内容的评论</span>
-      </div>
-    </div>
-    <div class="content">
-    <ul>
-      <li class="rate-item" v-for="(rate, index) in selectrating" v-bind:key="index">
-        <div class="rate-msg">
-          <div class="person-img" v-bind:style="{backgroundImage : 'url('+rate.avatar +')'}"></div>
-          <div class="person-msg">
-            <div class="person-name">{{rate.username}}</div>
-            <div class="rate-extra">
-              <star class="star" :size='36' :score=rate.score></star>
-              <div class="delivery-time" v-show="rate.deliveryTime!=''">{{rate.deliveryTime}}分钟送达</div>
-            </div>
+        <div class="overview-right">
+          <div class="food-score">
+            <div class="text">服务态度</div>
+            <star class="star" :size='36' :score=seller.serviceScore></star>
+            <div class="number">{{seller.serviceScore}}</div>
           </div>
-          <div class="rate-time">{{rate.rateTime | formatDate}}</div>
+          <div class="service-score">
+            <div class="text">食物评分</div>
+            <star class="star" :size='36' :score=seller.foodScore></star>
+            <div class="number">{{seller.serviceScore}}</div>
+          </div>
+          <div class="deliverytime">
+            <div class="text">送达时间</div>
+            <div class="time">{{seller.deliveryTime}}分钟</div>
+          </div>
         </div>
-        <div class="rate-content">
-          {{rate.text}}
+      </div>
+    </div>
+    <!-- 评论区域  -->
+    <div class="food-rating">
+      <div class="header">
+        <div class="btn-group">
+          <div class="all" @click="changeRate('all')">全部<span class="ratingtotal"></span></div>
+          <div class="recommand" @click="changeRate('recommand')">推荐</div>
+          <div class="gag" @click="changeRate('gag')">吐槽</div>
         </div>
-        <div class="rate-command" v-show="rate.recommend[0]!=''">
-          <ul>
-            <i v-bind:class="[rate.rateType === 0 ? 'icon-thumb_up' : 'icon-thumb_down']"></i>
-            <li v-for="(rec,index) in rate.recommend" v-bind:key="index" class="command-item">
-              {{rec}}
-            </li>
-          </ul>
+        <div class="ratingoption">
+          <i  class="icon-check_circle" v-bind:class="[rateoption.showcontent ? 'checked' : '']" @click="showContent()"></i><span>只看有内容的评论</span>
         </div>
-      </li>
-    </ul>
+      </div>
+      <div class="content">
+        <ul>
+          <li class="rate-item" v-for="(rate, index) in selectrating" v-bind:key="index">
+            <div class="rate-msg">
+              <div class="person-img" v-bind:style="{backgroundImage : 'url('+rate.avatar +')'}"></div>
+              <div class="person-msg">
+                <div class="person-name">{{rate.username}}</div>
+                <div class="rate-extra">
+                  <star class="star" :size='36' :score=rate.score></star>
+                  <div class="delivery-time" v-show="rate.deliveryTime!=''">{{rate.deliveryTime}}分钟送达</div>
+                </div>
+              </div>
+              <div class="rate-time">{{rate.rateTime | formatDate}}</div>
+            </div>
+            <div class="rate-content">
+              {{rate.text}}
+            </div>
+            <div class="rate-command" v-show="rate.recommend[0]!=''">
+              <ul>
+                <i v-bind:class="[rate.rateType === 0 ? 'icon-thumb_up' : 'icon-thumb_down']"></i>
+                <li v-for="(rec,index) in rate.recommend" v-bind:key="index" class="command-item">
+                  {{rec}}
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
-  </div>
-</div>
 </template>
 <script type="text/ecmascript-6">
 import {formatDate} from '../../common/js/date'
@@ -100,7 +100,7 @@ export default {
   },
   created () {
     this.$nextTick(() => {
-      this._initScroll()
+      // this._initScroll()
     })
     this.$http.get('api/ratings').then((request) => {
       request = request.data
--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,40 +1,78 @@
 <template>
-<div class="seller">
-  <div class="header">
-    <div class="notices">
-      <div class="notice">
-        <div class="header-left">
-          <div class="name">{{seller.name}}</div>
-          <div class="star">
-            <star class="star" :size='36' :score=seller.score></star>
-            <div class="sell-count">月售{{seller.sellCount}}单</div>
+<div class="seller" ref="sellWapper">
+  <div class="seller-content">
+    <div class="header">
+      <div class="notices">
+        <div class="notice">
+          <div class="header-left">
+            <div class="name">{{seller.name}}</div>
+            <div class="star">
+              <star class="star" :size='36' :score=seller.score></star>
+              <div class="sell-count">月售{{seller.sellCount}}单</div>
+            </div>
+          </div>
+          <div class="header-right">
+            <i class="icon-favorite"></i>
+            <div class="collect">
+              <span class="collect-text">已收藏</span>
+            </div>
           </div>
         </div>
-        <div class="header-right">
-          <i class="icon-favorite"></i>
-          <div class="collect">
-            <span class="collect-text">已收藏</span>
-          </div>
+      </div>
+      <div class="seller-extra">
+        <div class="minPrice">
+          <div class="title">起送价</div>
+          <div class="content">{{seller.minPrice}}<span class="small">元</span></div>
+        </div>
+        <div class="deliveryPrice">
+          <div class="title">商家配送</div>
+          <div class="content">{{seller.deliveryPrice}}<span class="small">元</span></div>
+        </div>
+        <div class="deliveryTime">
+          <div class="title">平均配送时间</div>
+          <div class="content">{{seller.deliveryTime}}<span class="small">分钟</span></div>
         </div>
       </div>
     </div>
-    <div class="seller-extra">
-      <div class="minPrice">
-        <div class="title">起送价</div>
-        <div class="content">{{seller.minPrice}}<span class="small">元</span></div>
+    <div class="special">
+      <div class="bulletin">
+        <div class="title">
+          公告与活动
+        </div>
+        <div class="content">
+          {{seller.bulletin}}
+        </div>
       </div>
-      <div class="deliveryPrice">
-        <div class="title">商家配送</div>
-        <div class="content">{{seller.deliveryPrice}}<span class="small">元</span></div>
+      <div class="supports">
+        <ul>
+          <li v-for="(item,index) in seller.supports" v-bind:key="index">
+            <div class="logo"><i class="icon" :class="classMap[item.type]"></i></div>
+            <div class="description">{{item.description}}</div>
+          </li>
+        </ul>
       </div>
-      <div class="deliveryTime">
-        <div class="title">平均配送时间</div>
-        <div class="content">{{seller.deliveryTime}}<span class="small">分钟</span></div>
+    </div>
+    <div class="picture">
+      <div class="title">商家实景</div>
+      <div class="content">
+        <ul>
+          <li v-for="(picitem,index) in seller.pics" v-bind:key="index">
+            <img :src="picitem">
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="information">
+      <div class="title">商家信息</div>
+      <div class="content">
+        <ul>
+          <li v-for="(info,index) in seller.infos" v-bind:key="index">
+            {{info}}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
-  <div class="picture"></div>
-  <div class="information"></div>
 </div>
 </template>
 <script type="text/ecmascript-6">
@@ -48,6 +86,19 @@ export default {
   },
   components: {
     star
+  },
+  created () {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+    this.$nextTick(() => {
+      // this._initScroll()
+    })
+  },
+  methods: {
+    // _initScroll () {
+    //   let sellerScroll = new BScroll(this.$refs.sellWapper, {
+    //     click: true
+    //   })
+    // }
   }
 }
 </script>
